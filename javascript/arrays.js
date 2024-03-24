0cf4e97145891789d9eb058e1e50faacc6755a13
Jeboy <tovilajesus@gmail.com>
2024-03-24 20:09:23 -0300

    Operadores logicos, Bucles y Arrays

---
+++ javascript/arrays.js
@@ -0,0 +1,73 @@
+ /*var listaDeCompras = [];
+ listaDeCompras[3] = "Tomates";
+ listaDeCompras[1] = "Lechuga";
+
+ console.log(listaDeCompras.length);
+ listaDeCompras[1];
+ var elementoDelArray = listaDeCompras[1];
+ console.log(elementoDelArray);
+
+ var colores = ["amarillo", "azul"];
+ colores.push("rojo");
+ colores.unshift("verde");
+ colores.pop();
+ colores.shift();
+ console.log(colores);
+
+ var pintores = ["Picasso", "Velazquez", 'Van Gogh', 'Dali'];
+ var incluyeDali = pintores.includes('Monet');
+ console.log(incluyeDali);
+
+ var numeros = [1, 6, 8, 9];
+ var cumpleCondicion = numeros.every((num) => {
+    return num > 5;
+ });
+ console.log(cumpleCondicion);
+
+ var palabra = 'Henri';
+ var palabraSeparada = palabra.split('');
+ palabraSeparada.pop();
+ palabraSeparada.push('y');
+ //console.log(palabraSeparada);
+ var palabraArreglada = palabraSeparada.join('');
+ console.log(palabraArreglada);
+
+ var numeros = [1, 2, 3, 4];
+ //numeros.forEach((num) => console.log(num));
+ //numeros.forEach((num) => {
+ //   if (num === 3) {
+ //       console.log(num);
+ //   }
+ //});
+
+ var masUno = numeros.map((num) => {
+    return num + 1;
+ });
+ console.log(masUno);
+
+ var arr = [1, 2, 3, 4, 5];
+
+ for (var i = 0; i < arr.length; i++) {
+    console.log(arr[i]);
+ }
+
+ function encontrarLetraP(string) {
+    var letras = string.split('');
+
+    for (let i = 0; i< letras.length; i++) {
+        if (letras[i] === 'p') {
+            console.log('Si contiene p');
+        }
+    }
+ }
+
+ encontrarLetraP('JavaScript');
+ encontrarLetraP('Henry');
+
+ var arr = [];
+
+ while (arr.length < 5) {
+    arr.push('BOOM');
+ }
+
+ console.log(arr);*/ 
